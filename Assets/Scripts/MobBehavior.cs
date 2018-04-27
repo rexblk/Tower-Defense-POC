3f005eb88da1775382f1fc3ad327a3b9e77f3b1d
Rex Black <RexNBlack@gmail.com>
2018-04-26 21:05:55 -0500

    first commit

---
+++ Assets/Scripts/MobBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobBehavior : MonoBehaviour {
+    public Transform DestinationPoint;
+	// Use this for initialization
+	void Start () {
+        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = DestinationPoint.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
