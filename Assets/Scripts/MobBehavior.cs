1958e9f0420bbda50c23457380bb12f4ee55a18b
Rex Black <RexNBlack@gmail.com>
2018-04-26 23:08:15 -0500

    added gitignore

--- Assets/Scripts/MobBehavior.cs
+++ Assets/Scripts/MobBehavior.cs
@@ -6,11 +6,12 @@ public class MobBehavior : MonoBehaviour {
     public Transform DestinationPoint;
 	// Use this for initialization
 	void Start () {
-        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = DestinationPoint.position;
-	}
+        var meshAgent = transform.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        meshAgent.destination = DestinationPoint.position;
+        meshAgent.autoRepath = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    }
 }
--- Assets/Scripts/Plot.cs
+++ Assets/Scripts/Plot.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Plot : MonoBehaviour {
+    public bool HasTower;
+    public GameObject Tower;
+    private Vector3 _position;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        _position = gameObject.transform.position;
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,7 +19,14 @@ public class Plot : MonoBehaviour {
 
     private void OnMouseDown()
     {
-        print("You clicked Plot " +  gameObject.transform.position.x + ", " + gameObject.transform.position.z);
+        print("You clicked Plot " +  _position.x + ", " + _position.z);
+        if (!HasTower)
+        {
+            HasTower = true;
+            var towerPosition = _position;
+            towerPosition.y++;
+            Instantiate(Tower, towerPosition, Quaternion.identity);
+        }
     }
 
 }
